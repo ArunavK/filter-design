649810f65f2641d966ff0d57bf91a63d7f3e321f
ArunavK <46145620+ArunavK@users.noreply.github.com>
2020-12-14 07:37:21 +0530

    Added frequency sampling method implementation
    Added a function frequencySampling(H, N) to find the N-order FIR filter impulse response h from samples of magnitude response H

---
+++ frequencySampling.m
@@ -0,0 +1,26 @@
+function [h] = frequencySampling(H, N)
+    tic;
+    H = H';
+    n = 0:N-1;
+    summation = zeros(1, N);
+    if mod(N, 2) == 0
+        
+        alpha = N/2 - 1;
+        for k=1:alpha
+            summation = summation + 2*abs(H(k+1))*cos(2*pi*k*(n-alpha)/N);
+        end
+        h = (1/N) * (H(0+1) + summation);
+    else
+        
+        alpha = (N-1) / 2;
+        for k=1:alpha
+            summation = summation + 2*abs(H(k+1))*cos(2*pi*k*(n-alpha)/N);
+        end
+        h = (1/N) * (H(0+1) + summation);
+        
+    end
+    fprintf("\n**************************************\n");
+    toc;
+    fprintf("\n**************************************\n");
+end
+
